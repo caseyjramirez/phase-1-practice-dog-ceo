eae016cb121062eef00ba4b7ae4e788d5b8a466d
caseyjramirez <caseyjramirez5@gmail.com>
2022-08-17 08:15:05 -0500

    solution making a server call

--- src/index.js
+++ src/index.js
@@ -1,1 +1,70 @@
-console.log('%c HI', 'color: firebrick')
+const d = document;
+const imgCont = d.getElementById('dog-image-container');
+const dogBreeds = d.getElementById('dog-breeds')
+const dropDown = d.getElementById('breed-dropdown');
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+const breedUrl = 'https://dog.ceo/api/breeds/list/all';
+
+/*======================== FETCH FUNCTIONS ========================*/
+function getResources(url) {
+    return fetch(url)
+    .then(res => res.json())
+}
+
+/*======================== RENDER FUNCTIONS ========================*/
+function renderDogImage(data) {
+    const img = d.createElement('img')
+    img.src = data
+    imgCont.append(img)
+}
+
+function renderDogBreeds(data) {
+    const li = d.createElement('li')
+    li.classList.add('breeds')
+    li.textContent = data
+    dogBreeds.append(li)
+}
+
+
+
+/*======================== FUNCTIONS ========================*/
+function getDogImages() {
+    getResources(imgUrl)
+    .then(data => data.message.forEach(renderDogImage));
+}
+
+function clearDogBreeds() {
+    dogBreeds.innerHTML = ''
+}
+
+function getDogBreeds() {
+    getResources(breedUrl)
+    .then(data => {
+        for (let breed in data.message) {
+            renderDogBreeds(breed)
+        }
+    })
+}
+
+function handleDropDownChange(e) {
+    const query = e.target.value;
+
+    
+    clearDogBreeds()
+    
+    getResources(breedUrl)
+    .then(data => {
+        for (let breed in data.message) {
+            if(breed[0] === query) {
+                renderDogBreeds(breed)
+            }
+        }
+    })
+}
+
+
+
+getDogImages()
+getDogBreeds()
+dropDown.addEventListener('change', handleDropDownChange);
